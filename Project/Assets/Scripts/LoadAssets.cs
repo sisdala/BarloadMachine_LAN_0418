0956f38718f84c7e9d0f49483dc0f4743316832a
Ting <oitcappteam@gmail.com>
2017-04-11 21:25:25 +0800

    55555

---
+++ Project/Assets/Scripts/LoadAssets.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadAssets : MonoBehaviour {
+	public AssetBundle Abs;
+	public string Name;
+	public Object Child;
+
+
+	void gogogo()
+	{
+		Child = Instantiate (Abs.LoadAsset (Name));
+		Debug.Log ("5555555555");
+	}
+}
