c3d4d66f02315a7d1ab28f8af04be065848298f1
htw75 <htw750128@gmail.com>
2017-04-12 23:52:35 +0800

    把整個專案在上船上去

---
+++ Project/Assets/Editor/BuildABs.cs
@@ -0,0 +1,20 @@
+﻿// Create an AssetBundle for Android.
+using UnityEngine;
+using UnityEditor;
+
+public class BuildAssetBundlesExample : MonoBehaviour
+{
+	[MenuItem( "Assets/Build Asset Bundles/Android" )]
+	static void BuildAssetAndroid( )
+	{
+		// 輸入打包路徑
+		BuildPipeline.BuildAssetBundles( "AssetBundles/Android", BuildAssetBundleOptions.None, BuildTarget.Android );
+	}
+
+	[MenuItem( "Assets/Build Asset Bundles/Windows" )]
+	static void BuildAssetWindows( )
+		{
+		// 輸入打包路徑
+		BuildPipeline.BuildAssetBundles( "AssetBundles/Windows", BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows );
+	}
+}
---
+++ Project/Assets/Scripts/LoadAssets.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadAssets : MonoBehaviour {
+	public AssetBundle Abs;
+	public string Name;
+	public GameObject Child;
+
+
+	void Process()
+	{
+		Child = Instantiate (Abs.LoadAsset (Name))as GameObject;
+		Debug.Log ("5555555555");
+	}
+}
